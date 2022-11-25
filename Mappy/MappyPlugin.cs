1268172764e959530436afd49d8d01e99dc2c404
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-11-24 21:42:17 -0800

    Add project files.

---
+++ Mappy/MappyPlugin.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Plugin;
+
+namespace Mappy;
+
+public sealed class MappyPlugin : IDalamudPlugin
+{
+    public string Name => "Mappy";
+
+    public MappyPlugin(DalamudPluginInterface pluginInterface)
+    {
+        //pluginInterface.Create<Service>();
+    }
+
+    public void Dispose()
+    {
+    }
+}
