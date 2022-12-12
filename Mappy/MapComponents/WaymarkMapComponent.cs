c072ec16726617622b7bafb6772585ceb4f8d93d
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-11 23:51:51 -0800

    Use Lumina to get Field Marker IconID's for waymarks

--- Mappy/MapComponents/WaymarkMapComponent.cs
+++ Mappy/MapComponents/WaymarkMapComponent.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using Mappy.DataModels;
 using Mappy.Interfaces;
 using Mappy.Utilities;
+using FieldMarker = Lumina.Excel.GeneratedSheets.FieldMarker;
 
 namespace Mappy.MapComponents;
 
@@ -16,7 +17,14 @@ public class WaymarkSettings
 public class WaymarkMapComponent : IMapComponent
 {
     private static WaymarkSettings Settings => Service.Configuration.Waymarks;
+
+    private readonly List<FieldMarker> fieldMarkers;
     
+    public WaymarkMapComponent()
+    {
+        fieldMarkers = Service.DataManager.GetExcelSheet<FieldMarker>()!.Where(row => row.RowId is >= 1u and <= 8u).ToList();
+    }
+
     public void Update(uint mapID)
     {
         
@@ -27,11 +35,11 @@ public class WaymarkMapComponent : IMapComponent
         if (!Settings.Enable.Value) return;
         if (!Service.MapManager.PlayerInCurrentMap) return;
 
-        var fieldMarkers = MarkingController.Instance()->FieldMarkerSpan;
+        var markerSpan = MarkingController.Instance()->FieldMarkerSpan;
 
         foreach (var index in Enumerable.Range(0, 8))
         {
-            if (fieldMarkers[index] is { Active: true } marker)
+            if (markerSpan[index] is { Active: true } marker)
             {
                 var position = Service.MapManager.GetObjectPosition(marker.Position);
                     
@@ -40,19 +48,5 @@ public class WaymarkMapComponent : IMapComponent
         }
     }
 
-    private uint GetIconForMarkerIndex(int index)
-    {
-        return index switch
-        {
-            0 => 60474,
-            1 => 60475,
-            2 => 60476,
-            3 => 60936,
-            4 => 60931,
-            5 => 60932,
-            6 => 60933,
-            7 => 63904,
-            _ => throw new IndexOutOfRangeException()
-        };
-    }
+    private uint GetIconForMarkerIndex(int index) => fieldMarkers[index].MapIcon;
 }
