2c38c4abb3a61c8eb921519f96d03c8adf4a2242
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-13 23:06:43 -0800

    Validate goto command coordinates before centering viewport

--- Mappy/System/Commands/GotoCommand.cs
+++ Mappy/System/Commands/GotoCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Numerics;
+using Dalamud.Utility;
 using Mappy.Interfaces;
 using Mappy.Localization;
 using Mappy.Utilities;
@@ -27,12 +28,26 @@ public class GotoCommand : IPluginCommand
             var x = float.Parse(coordinateStrings[0]);
             var y = float.Parse(coordinateStrings[1]);
             
-            var worldX = ConvertMapToWorld(x, Service.MapManager.Map?.SizeFactor ?? 100u, Service.MapManager.Map?.OffsetX ?? 0);
-            var worldY = ConvertMapToWorld(y, Service.MapManager.Map?.SizeFactor ?? 100u, Service.MapManager.Map?.OffsetY ?? 0);
+            var textureSize = Service.MapManager.MapTextureSize;
 
-            MapRenderer.SetViewportCenter(new Vector2(worldX, worldY));
-            MapRenderer.SetViewportZoom(2.0f);
+            if (Service.MapManager.Map is { } map)
+            {
+                var mapMax = MapUtil.WorldToMap(textureSize, map);
+            
+                var worldX = ConvertMapToWorld(x, Service.MapManager.Map?.SizeFactor ?? 100u, Service.MapManager.Map?.OffsetX ?? 0);
+                var worldY = ConvertMapToWorld(y, Service.MapManager.Map?.SizeFactor ?? 100u, Service.MapManager.Map?.OffsetY ?? 0);
+
+                if (worldX <= mapMax.X && worldY <= mapMax.Y && worldX >= 1.0f && worldY >= 1.0f)
+                {
+                    MapRenderer.SetViewportCenter(new Vector2(worldX, worldY));
+                    MapRenderer.SetViewportZoom(2.0f);
+                    Chat.Print("Command", "Command Successful");
+                    return;
+                }
+            }
         }
+        
+        Chat.Print("Command", "Error or out of range");
     }
 
     private static float ConvertMapToWorld(float value, uint scale, int offset)
