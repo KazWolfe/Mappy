aad82b228192abc6747ef23f7500a4a180f5490e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-15 02:28:28 -0800

    Improve QuestExtended.cs
    Removed base implementation, and instead copied bases implementation into QuestExtended

--- Mappy/DataModels/QuestExtended.cs
+++ Mappy/DataModels/QuestExtended.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Runtime.InteropServices;
-using FFXIVClientStructs.FFXIV.Client.Game;
 
 namespace Mappy.DataModels;
+
+[Flags]
+public enum QuestFlags : byte
+{
+    None = 0,
+    Priority = 1,
+    Hidden = 8,
+}
 
 [StructLayout(LayoutKind.Explicit, Size = 24)]
 public struct QuestExtended
 {
-    [FieldOffset(0)] public QuestManager.QuestListArray.Quest Base;
+    [FieldOffset(8)] public ushort QuestID;
     [FieldOffset(10)] public readonly byte CurrentSequenceNumber;
+    [FieldOffset(11)] public QuestFlags Flags;
     [FieldOffset(17)] private readonly byte TodoBits;
 
+    public bool IsHidden => Flags.HasFlag(QuestFlags.Hidden);
     public BitArray TodoMask => new BitArray(new[]{TodoBits}).Reverse();
-
+    
+    public uint IconID => CurrentSequenceNumber == 0xFF ? 071025u : 071023u;
 }
 
 public static class BitArrayExtensions
