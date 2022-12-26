f9e988ab240b50e26ddbcaa44baad2666efaacff
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-25 16:05:31 -0800

    Remove previous version services

--- Mappy/MappyPlugin.cs
+++ Mappy/MappyPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using Dalamud.Plugin;
+using Mappy.Config;
 using Mappy.System;
 
 namespace Mappy;
@@ -14,36 +15,14 @@ public sealed class MappyPlugin : IDalamudPlugin
         Service.Configuration = Service.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
         Service.Configuration.Initialize(Service.PluginInterface);
 
-        // Load Localization First
-        Service.Localization = new LocalizationManager();
-        Service.Penumbra = new PenumbraIntegration();
-        
-        // Load Caches Next
-        Service.Cache = new CompositeLuminaCache();
 
-        // Load Non Critical Managers
-        Service.ModuleManager = new ModuleManager();
-        Service.QuestManager = new QuestManager();
-        
-        Service.Teleporter = new TeleportManager();
-        Service.MapManager = new MapManager();
-        Service.ContextMenu = new MapContextMenu();
-        
-        // Load Critical Managers
         Service.CommandManager = new CommandManager();
         Service.WindowManager = new WindowManager();
-        Service.GameIntegration = new GameIntegration();
     }
 
     public void Dispose()
     {
-        Service.Localization.Dispose();
-        Service.Teleporter.Dispose();
-        Service.CommandManager.Dispose();
         Service.WindowManager.Dispose();
-        Service.MapManager.Dispose();
-        Service.Cache.Dispose();
-        Service.GameIntegration.Dispose();
-        Service.ModuleManager.Dispose();
+        Service.CommandManager.Dispose();
     }
 }
--- Mappy/Service.cs
+++ Mappy/Service.cs
@@ -1,45 +1,22 @@
-﻿using Dalamud.Data;
-using Dalamud.Game;
-using Dalamud.Game.ClientState;
-using Dalamud.Game.ClientState.Aetherytes;
-using Dalamud.Game.ClientState.Objects;
-using Dalamud.Game.ClientState.Party;
+﻿using Dalamud.Game.ClientState;
 using Dalamud.Game.Gui;
-using Dalamud.Game.Gui.Toast;
 using Dalamud.IoC;
 using Dalamud.Plugin;
+using Mappy.Config;
 using Mappy.System;
-using CommandManager = Dalamud.Game.Command.CommandManager;
-using Condition = Dalamud.Game.ClientState.Conditions.Condition;
+using DalamudCommandManager = Dalamud.Game.Command.CommandManager;
 
 namespace Mappy;
 
 internal class Service
 {
     [PluginService] public static DalamudPluginInterface PluginInterface { get; private set; } = null!;
-    [PluginService] public static CommandManager Commands { get; private set; } = null!;
-    [PluginService] public static ChatGui Chat { get; private set; } = null!;
+    [PluginService] public static DalamudCommandManager Commands { get; private set; } = null!;
     [PluginService] public static ClientState ClientState { get; private set; } = null!;
-    [PluginService] public static DataManager DataManager { get; private set; } = null!;
-    [PluginService] public static ObjectTable ObjectTable { get; private set; } = null!;
-    [PluginService] public static Framework Framework { get; private set; } = null!;
-    [PluginService] public static Condition Condition { get; private set; } = null!;
-    [PluginService] public static AetheryteList AetheryteList { get; private set; } = null!;
-    [PluginService] public static ToastGui Toast { get; private set; } = null!;
-    [PluginService] public static PartyList PartyList { get; private set; } = null!;
+    [PluginService] public static ChatGui Chat { get; private set; } = null!;
+    [PluginService] public static GameGui GameGui { get; private set; } = null!;
 
-    public static CompositeLuminaCache Cache = null!;
-    
     public static Configuration Configuration = null!;
     public static WindowManager WindowManager = null!;
-    public static System.CommandManager CommandManager = null!;
-    public static TeleportManager Teleporter = null!;
-    public static LocalizationManager Localization = null!;
-    public static PenumbraIntegration Penumbra = null!;
-    
-    public static MapManager MapManager = null!;
-    public static GameIntegration GameIntegration = null!;
-    public static MapContextMenu ContextMenu = null!;
-    public static ModuleManager ModuleManager = null!;
-    public static QuestManager QuestManager = null!;
+    public static CommandManager CommandManager = null!;
 }
