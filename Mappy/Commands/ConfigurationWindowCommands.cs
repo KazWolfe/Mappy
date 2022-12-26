17a4cfdc6e6b2d9e84744f5ff54f4fbda1822293
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-25 16:09:04 -0800

    Add basic commands

---
+++ Mappy/Commands/ConfigurationWindowCommands.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Mappy.Interfaces;
+using Mappy.UI;
+using Mappy.Util;
+
+namespace Mappy.Commands;
+
+internal class ConfigurationWindowCommands : IPluginCommand
+{
+    public string? CommandArgument => null;
+
+    public IEnumerable<ISubCommand> SubCommands { get; } = new List<ISubCommand>
+    {
+        new SubCommand
+        {
+            CommandKeyword = null,
+            CommandAction = () => Chat.PrintError("The configuration menu cannot be opened while in a PvP area"),
+            CanExecute = () => Service.ClientState.IsPvP,
+            GetHelpText = () => "Open Configuration Window"
+        },
+        new SubCommand
+        {
+            CommandKeyword = null,
+            CommandAction = () =>
+            {
+                if ( Service.WindowManager.GetWindowOfType<ConfigWindow>() is {} mainWindow )
+                {
+                    mainWindow.IsOpen = !mainWindow.IsOpen;
+                }
+            },
+            CanExecute = () => !Service.ClientState.IsPvP,
+            GetHelpText = () => "Open Configuration Window"
+        },
+    };
+}
---
+++ Mappy/UI/ConfigWindow.cs
@@ -0,0 +1,17 @@
+﻿using Dalamud.Interface.Windowing;
+using ImGuiNET;
+
+namespace Mappy.UI;
+
+public class ConfigWindow : Window
+{
+    public ConfigWindow() : base("Mappy Configuration")
+    {
+        
+    }
+
+    public override void Draw()
+    {
+        
+    }
+}
