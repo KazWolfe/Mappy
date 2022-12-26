d65a08124a07345f8a4d6d52b55d7394bdb76780
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-12-25 16:04:37 -0800

    Increase Configuration.cs version

---
+++ Mappy/Config/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Dalamud.Configuration;
+using Dalamud.Plugin;
+
+namespace Mappy.Config;
+
+[Serializable]
+public class Configuration : IPluginConfiguration
+{
+    public int Version { get; set; } = 2;
+    
+    
+    [NonSerialized]
+    private DalamudPluginInterface? pluginInterface;
+    public void Initialize(DalamudPluginInterface inputPluginInterface) => pluginInterface = inputPluginInterface;
+    public void Save() => pluginInterface!.SavePluginConfig(this);
+}
